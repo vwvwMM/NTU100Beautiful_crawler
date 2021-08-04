3f623f096c7417bdadae7de49a718cd34f391a38
vwvwMM <vincentwu0628@gmail.com>
2021-08-05 01:34:54 +0800

    create posts_to_scrape

--- mbasic_login_version.py
+++ mbasic_login_version.py
@@ -8,6 +8,7 @@ LOGIN_URL = 'https://mbasic.facebook.com/login/device-based/regular/login/?refsr
 USERNAME = 'ttttrickortreat@gmail.com'
 PASSWORD = 'Eric0620eric'
 
+posts_to_scrape=5
 
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36 Edg/92.0.902.62'
 session = requests.Session()
@@ -15,7 +16,7 @@ session = requests.Session()
 
 def LOGGER(username, password):
     req = session.get(BASE_URL)
-    soup = BeautifulSoup(req.text, 'html5lib')
+    soup = BeautifulSoup(req.text, 'html.parser')
     '''
     refs = soup.find_all('a')
     ref = refs[-1]['href']
@@ -23,7 +24,7 @@ def LOGGER(username, password):
     print(ref)
     '''
     # login
-    soup = BeautifulSoup(req.text, 'html5lib')
+    soup = BeautifulSoup(req.text, 'html.parser')
     LSD = soup.find('input', {'name': 'lsd'})['value']
     JAZO = soup.find('input', {'name': 'jazoest'})['value']
     MTS = soup.find('input', {'name': 'm_ts'})['value']
@@ -39,7 +40,7 @@ def LOGGER(username, password):
         'pass': password
     }
     resp = session.post(LOGIN_URL, data=form_data)
-    soup = BeautifulSoup(resp.text, 'html5lib')
+    soup = BeautifulSoup(resp.text, 'html.parser')
     return soup
 # loged in
 # access
@@ -48,7 +49,7 @@ def LOGGER(username, password):
 def post_scraper(search_url, query_params):
 
     resp = session.get(search_url, params=query_params)
-    soup = BeautifulSoup(resp.text, 'html5lib')
+    soup = BeautifulSoup(resp.text, 'html.parser')
     IMFS = {}
     if soup.find('h3'):
         IMFS[0] = soup.find('div', {'id': 'objects_container'}).find_all('a')[
@@ -61,7 +62,7 @@ def post_scraper(search_url, query_params):
     img_url = soup.paragraph = soup.find('div', {'id': 'objects_container'}).find_all(
         'a')[1].parent.parent.parent.parent.next_sibling.find_all('a')[1]['href']
     resp = session.get(BASE_URL+img_url)
-    soup = BeautifulSoup(resp.text, 'html5lib')
+    soup = BeautifulSoup(resp.text, 'html.parser')
     img_count = 1
     while True:
         if not soup.find_all('img')[1]['src'] in IMFS.values():
@@ -70,7 +71,7 @@ def post_scraper(search_url, query_params):
             img_url = soup.find_all(
                 'img')[1].parent.parent.next_sibling.find_all('a')[1]['href']
             resp = session.get(BASE_URL+img_url)
-            soup = BeautifulSoup(resp.text, 'html5lib')
+            soup = BeautifulSoup(resp.text, 'html.parser')
             img_count += 1
         else:
             print('all found')
@@ -95,7 +96,7 @@ def full_auto():
     }
     session.headers.update({'authority': 'mbasic.facebook.com'})
     resp = session.post(LOGIN_URL, data=form_data)
-    soup = BeautifulSoup(resp.text, 'html5lib')
+    soup = BeautifulSoup(resp.text, 'html.parser')
     st = soup.find('a')['href']
     num = 0
     if str.split('refid')[-1]:
@@ -105,7 +106,7 @@ def full_auto():
     STRING = str(STRING.encode('utf-8'))[2:-1]
     # search
     # the range of searching posts
-    for i in range(1, 10):
+    for i in range(1, posts_to_scrape):
         NUMBER = str(i)
         STRING = STRING.replace('\\x', '%')
         SEARCH_URL = 'https://mbasic.facebook.com/hashtag/'+'台大百大正妹ntu100beauty'+NUMBER
@@ -132,11 +133,11 @@ PARAMS = {
 session.headers.update({'referer': BASE_URL})
 resp = session.get(
     'https://mbasic.facebook.com/search/top/?q=%E5%B7%AB%E7%AB%91%E5%84%92&refid={}&_rdr', params=PARAMS)
-soup = BeautifulSoup(resp.text, 'html5lib')
+soup = BeautifulSoup(resp.text, 'html.parser')
 PROFILE = soup.find('div', {'id': 'BrowseResultsContainer'}).find_all('a')[
     0]['href']
 resp = session.get(BASE_URL+PROFILE)
-soup = BeautifulSoup(resp.text, 'html5lib')
+soup = BeautifulSoup(resp.text, 'html.parser')
 
 resp = session.get('https://mbasic.facebook.com/reactions/picker/?ft_id=1098809383962859&origin_uri=https%3A%2F%2Fmbasic.facebook.com%2Fprofile.php%3Fid%3D100015014234107%26refid%3D46%26__xts__%255B0%255D%3D12.Abq5ICqZ2cYuvNSa7wIw3iwhghzAKVH51lCRvTnOn9ed1NVgMdyrpB_sDzx2cYUY3yRrqPqlhRHiO3u1UWk-Lam07wq0dA8nb-b9266sS_N8CrIl6kflw-368zmhT6vBfpsEqiq1TDMHzEO7w37DuylzybHwpL7Iclw6IXbGFaJkWUNPlsRX_n7JFyHP8CoutBloQWD-kAMh1FQ75YrZrbuw03ketzzSjdlr5rhLsIMgwC6Ufw0Shtzrdlw9hbK1-HJm3jXRNdBPFd5Il2N3nezxZvzhEfbUFuPLMMO2ijAlgrzj_X3f0S_SroCv7cNp7nK-l3MeNDcY_hHdd4EuKKE9AF_tg1qGUzg_TEv5darmmNb3nbFV6Qtk3IO9EqIG6roo1ICHrmGSGISio0cJI6F7_F3uJ1HBRBZcbsDDKJNzS4c38XBxfGjAPnPnBNJFB8n-WkLpPWsvODmOzLa26eufOilnCnjPKRPqa7o-7tAIZAC0GsY3kLdec7sGvq6U6Vx8uvesPyz4lwanwviv_Rgsz8sy04uQsYNNSsxlq9-NuNJYcM3_zhbqi5Bv7xT60CU%26fbt_id%3D1098809383962859%26lul&_ft_=mf_story_key.1098809383962859%3Atop_level_post_id.1098809383962859%3Atl_objid.1098809383962859%3Acontent_owner_id_new.100015014234107%3Athrowback_story_fbid.1098809383962859%3Apage_id.1747372898921603%3Aphoto_attachments_list.%5B1098803530630111%2C1098807870629677%2C1098808580629606%5D%3Astory_location.4%3Astory_attachment_style.album%3Atds_flgs.3%3Aott.AX9uAnNvq21ueBPQ%3Athid.100015014234107%3A306061129499414%3A2%3A0%3A1630479599%3A1777496805958400870&av=100007401426788&refid=17&__tn__=%2AW-R')
 print(resp.text)
