a5d1ed4fdf8a8e386e9b3163ad5198a42a0bd73d
vwvwMM <vincentwu0628@gmail.com>
2021-08-05 00:44:21 +0800

    add flask template

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, render_template, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///posts.db'
+db=SQLAlchemy(app)
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
